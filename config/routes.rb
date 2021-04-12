b30cd0f076934f6c3c2abf871318b77c7f99fea4
André Nery <martarcnery@gmail.com>
2021-04-12 23:41:26 +0100

    Tradução dos menus a funcionar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do  
+  
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+    devise_for :users
+    resources :users
+    root to: 'pages#home'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+
 end
